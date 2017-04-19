c34bd4b89bac8d3fecf5b254eff1b68b6fce73e6
kruglay <kruglay@mail.ru>
2017-04-19 21:36:14 +0300

    initialize

---
+++ 8ball.rb
@@ -0,0 +1,39 @@
+greetings =[
+    "Who asks, he will receive the answer:\n",
+    "Hello, mortal. The answer for you today is\n",
+    "Hello, dear friend. As for your question ...\n"
+]
+
+answers = [
+# Positive
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+
+    # Neutral
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+
+    # Negative
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+]
+
+puts greetings.sample
+
+sleep 2
+
+puts answers.sample
---
+++ Readme.md
@@ -0,0 +1,11 @@
+##Game 8ball
+
+Ruby training application.
+The Magic 8-Ball is a toy used for fortune-telling or seeking advice. For more information see https://en.wikipedia.org/wiki/Magic_8-Ball
+
+####Usage
+
+Clone repository. Put in console
+```
+ ruby 8ball.rb
+```
